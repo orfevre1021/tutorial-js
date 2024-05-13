b084a7b03b98bbb6dce8c3d7078e29f7b76cc562
Your Name <you@example.com>
2024-05-14 03:01:16 +0900

    api追加

--- skills-dashboard/pages/users.jsx
+++ skills-dashboard/pages/users.jsx
@@ -4,7 +4,9 @@ import { user_data } from "../data/user_data.jsx";
 import Header from "../components/Header.jsx";
 import Link from "next/link";
 
-const users = () => {
+const EmployeeList = () => {
+  const [employees, setEmployees] = useState([]);
+
   const [dropdownOpen, setDropdownOpen] = useState(null);
   const dropdownRef = useRef(null); // ドロップダウンメニューを参照するためのref
 
@@ -24,10 +26,25 @@ const users = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      try {
+        const response = await fetch(
+          "https://yurdpuchaa.execute-api.ap-northeast-1.amazonaws.com/dev3/users"
+        );
+        const data = await response.json();
+        setEmployees(data.Items);
+      } catch (error) {
+        console.error("Failed to fetch data:", error);
+      }
+    };
+
+    fetchEmployees();
+  }, []);
+
   return (
     <div className="bg-gray-100 min-h-screen">
       <Header title="ユーザ一覧" user="Tanaka" />
-
       <div className="p-4">
         <div className="w-full m-auto p-4 border rounded-lg bg-white overflow-y-auto">
           <div className="my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer">
@@ -37,9 +54,9 @@ const users = () => {
             <span className="hidden sm:grid">最新更新</span>
           </div>
           <ul>
-            {user_data.map((user, id) => (
+            {employees.map((item, index) => (
               <li
-                key={id}
+                key={index}
                 className="bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer"
               >
                 <div className="flex items-center">
@@ -48,48 +65,51 @@ const users = () => {
                   </div>
                   <div className="pl-4">
                     <p>
-                      {user.user_name}
+                      {item.user_name.S}
                       <br></br>
                     </p>
                     <p className="text-gray-400">
                       {"（"}
-                      {user.employee_code}
+                      {item.employee_code.N}
                       {"）"}
                     </p>
                   </div>
                 </div>
+
                 <p className="hidden md:flex">
-                  {user.department} {user.division}
+                  {item.department.S} {item.division.S} {item.position.S}
                 </p>
-                <p className="sm:text-left text-right">{user.email_address}</p>
+                <p className="sm:text-left text-right">
+                  {item.email_address.S}
+                </p>
 
                 <div className="sm:flex hidden justify-between items-center">
-                  <p>{user.updated_date}</p>
+                  <p>{item.updated_date.S}</p>
                   <BsThreeDotsVertical
-                    onClick={() => setDropdownOpen(user.employee_code)}
+                    onClick={() => setDropdownOpen(item.employee_code.N)}
                   />
-                  {dropdownOpen === user.employee_code && (
+                  {dropdownOpen === item.employee_code.N && (
                     <div
                       ref={dropdownRef}
                       className="absolute mt-12 right-0 bg-white shadow-lg ring-1 ring-black ring-opacity-5"
                     >
                       <ul>
                         <li>
-                          <Link href={`/view/${user.employee_code}`}>
+                          <Link href={`/view/${item.employee_code.N}`}>
                             <div className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
                               閲覧
                             </div>
                           </Link>
                         </li>
                         <li>
-                          <Link href={`/edit/${user.employee_code}`}>
+                          <Link href={`/edit/${item.employee_code.N}`}>
                             <div className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
                               編集
                             </div>
                           </Link>
                         </li>
                         <li>
-                          <Link href={`/delete/${user.employee_code}`}>
+                          <Link href={`/delete/${item.employee_code.N}`}>
                             <div className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
                               削除
                             </div>
@@ -108,4 +128,4 @@ const users = () => {
   );
 };
 
-export default users;
+export default EmployeeList;
